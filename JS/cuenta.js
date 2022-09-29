26170ad2717a14df93eb6956785b1f1463d13956
Franco Silva <franco.silva4477@alumnos.udg.mx>
2022-09-28 21:16:34 -0500

    Base de datos, usuario y admin
    Se añadieron libros, se crearon las paginas para el usuario y el admin asi como el pago a proveedores

--- JS/cuenta.js
+++ JS/cuenta.js
@@ -3,13 +3,37 @@ async function validar(){
     const json = await response.json();
     user = document.getElementById("username").value;
     pass = document.getElementById("password").value;
+    const object = `{
+        "user" : "${user}",
+        "pass" : "${pass}"
+    }`;
+    const obj = JSON.parse(object);
+    console.log(obj);
     for (var n in json.cuentas){
         if (json.cuentas[n].user==user){
             if(json.cuentas[n].pass==pass){
                 //acceso
                 console.log("si sos");
+                document.cookie = obj;
+                console.log("cookie");
+                console.log(obj);
+                if(user=="Franco")
+                {
+                var w=window.open("usuario.html");
+                w.user=user;
+                w.pass=pass;
+                }
+                else{
+                    var w=window.open("admin.html");
+                    w.user=user;
+                    w.pass=pass;
+                }
             }
         }
     }
     
+}
+function onloadC(user){
+    html=`${user}`;
+    document.getElementById("nombre-cuenta").innerHTML = html;
 }
--- JS/datos.js
+++ JS/datos.js
@@ -74,4 +74,51 @@ var libros = datos =>{
 document.getElementById("caja-libro-carrito").innerHTML = html;
 }
 
+var app ={};
+var romance = datos =>{
+    app.libros=datos;
+    var html = ""
+    app.libros.map(libro=>{
+        html+=`
+        <div class="product-card">
+            <div class="product-image">
+                    <img src=${libro.img} class="product-thumb" alt="" id="${libro.id}" onclick="abrir(this)">
+                <button class="card-btn">Agregar al carrito</button>
+        </div>
+        <div class="product-info">
+            
+                <h2 class="product-brand" id="${libro.id}" onclick="abrir(this)">${libro.nombre}</h2>
+          
+            <p class="product-short-description">${libro.autor}</p>
+            <span class="price">${libro.precio}</span>
+        </div>
+    </div>
+    `;
+    })
+document.getElementById("caja-libro-romance").innerHTML = html;
+}
+
+var app ={};
+var comic = datos =>{
+    app.libros=datos;
+    var html = ""
+    app.libros.map(libro=>{
+        html+=`
+        <div class="product-card">
+            <div class="product-image">
+                    <img src=${libro.img} class="product-thumb" alt="" id="${libro.id}" onclick="abrir(this)">
+                <button class="card-btn">Agregar al carrito</button>
+        </div>
+        <div class="product-info">
+            
+                <h2 class="product-brand" id="${libro.id}" onclick="abrir(this)">${libro.nombre}</h2>
+          
+            <p class="product-short-description">${libro.autor}</p>
+            <span class="price">${libro.precio}</span>
+        </div>
+    </div>
+    `;
+    })
+document.getElementById("caja-libro-comic").innerHTML = html;
+}
 
