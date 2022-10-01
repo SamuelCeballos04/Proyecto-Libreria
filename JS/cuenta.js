0a3c0bb51e9bf7f183ec0d055a41e7435cc4a1bf
Franco Silva <franco.silva4477@alumnos.udg.mx>
2022-09-30 19:08:53 -0500

    Paginas personalizadas para cada usuario
    Ya carga las paginas con los datos de los usuarios

--- JS/cuenta.js
+++ JS/cuenta.js
@@ -9,6 +9,7 @@ async function validar(){
     }`;
     const obj = JSON.parse(object);
     console.log(obj);
+    console.log("aaaa");
     for (var n in json.cuentas){
         if (json.cuentas[n].user==user){
             console.log("si");
@@ -38,13 +39,47 @@ function onloadC(user, pass){
     console.log(user);
     html=`${user}`;
     document.getElementById("nombre-cuenta").innerHTML = html;
+}
+
+function onloadDC(user,pass,direccion,telefono){
+    html=`${user}`;
+    document.getElementById("nombre-cuenta").innerHTML = html;
     html= `<h1>Bienvenido!</h1>
-    <p id="nombre-c">${user}</p>
+    <p id="nombre-c">Nombre: ${user}</p>
     <p id="direccion-c">Direccion: direccion</p>
     <p id="telefono-c">Telefono: Telefono</p>
     <a href="index.html">
     <button type="button" class="btn btn-primary">Salir</button>
     </a>`;
     document.getElementById("datos-usuario").innerHTML=html;
+}
+
+async function detallesC(user,pass){
+    const response = await fetch("JSON/cuentas.json");
+    const json = await response.json();
+    for (var n in json.cuentas){
+        if (json.cuentas[n].user==user){
+            console.log("si");
+            if(json.cuentas[n].pass==pass){
+                //acceso
+                console.log("si sos");
+                if(user=="admin")
+                {
+                var w=window.open("datosadmin.html");
+                w.user=user;
+                w.pass=pass;
+                w.direccion = json.cuentas[n].direccion;
+                w.telefono = json.cuentas[n].telefono;
+                }
+                else{
+                    var w=window.open("datosuser.html");
+                    w.user=user;
+                    w.pass=pass;
+                    w.direccion = json.cuentas[n].direccion;
+                    w.telefono = json.cuentas[n].telefono;
+                }
+            }
+        }
+    }
 }
 
